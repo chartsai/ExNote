75a2b61b59ac42f37ae2215d111136c7b6ed18fe
CHA-MBP <cha122977@CHA-MBP.local>
2012-06-03 13:05:23 +0800

    test commit two

--- src/com/ntu/iddc/exnote/ExNoteActivity.java
+++ src/com/ntu/iddc/exnote/ExNoteActivity.java
@@ -2,10 +2,12 @@ package com.ntu.iddc.exnote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExNoteActivity extends Activity {
 	TextView tv;
+	EditText et;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
